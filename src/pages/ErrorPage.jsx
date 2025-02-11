717ddf1c98de6562c6183ea56c1c1427dcf03bb3
Soraiya11-7 <soraiyaparvin@gmail.com>
2025-02-11 23:40:13 +0600

    Updated 404 page with React Lottie animations for improved user interaction

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,17 +1,20 @@
 import { Link, useRouteError } from "react-router-dom";
-
+import lotti from "../assets/notFound.json"
+import Lottie from "lottie-react";
 const ErrorPage = () => {
     const error = useRouteError();
     // console.log(error);
+    
 
     return (
-        <div className="text-center flex flex-col justify-center items-center mt-20 container w-[90%] mx-auto">
-            
-            <p className="text-3xl sm:text-5xl font-bold mb-3 sm:mb-5">
+        <div className=" dark:text-white text-center flex flex-col justify-center items-center h-screen container w-[90%] mx-auto">
+         
+            <Lottie animationData={lotti} loop={true} className=" " />
+            <p className="text-3xl sm:text-4xl md:text-5xl font-bold mb-3 -mt-12 sm:-mt-16 z-10">
                 <i>{error.statusText || error.message}</i>
             </p>
-            <h5 className="mb-5 text-xl sm:text-3xl">Go back where you from</h5>
-             <Link to="/"><button className="btn bg-sky-400 text-white ">Home</button></Link>
+            <h5 className="mb-3 text-xl sm:text-2xl md:text-3xl z-10">Go back where you from</h5>
+             <Link to="/"><button className="text-white px-4 md:px-6 py-2 rounded-full text-base md:text-xl hover:bg-sky-600 transition duration-300 bg-sky-500 font-semibold z-10 ">Home</button></Link>
           
         </div>
     );
